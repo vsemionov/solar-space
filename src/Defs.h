a82547b270c486709b5ffdc5fa5b31d1f301c869
Victor Semionov <vsemionov@gmail.com>
2011-05-17 16:06:23 +0300

    Normalized include guards.

--- src/Defs.h
+++ src/Defs.h
@@ -25,8 +25,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef defs_h_incl
-#define defs_h_incl
+#ifndef DEFS_H
+#define DEFS_H
 
 
 #ifndef __cplusplus
--- src/Tensor.h
+++ src/Tensor.h
@@ -25,6 +25,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifndef TENSOR_H
+#define TENSOR_H
+
 typedef	double	Vector[3];
 typedef	double	Matrix[3][3];
 
@@ -34,3 +37,5 @@ extern	void	VecCross(Vector a, Vector b, Vector c);
 extern	void	MatVecMult(Matrix m, Vector v, Vector r);
 extern	void	MatMult(Matrix a, Matrix b, Matrix c);
 extern	void	Matrix3D(float m4d[16], Matrix m3d);
+
+#endif
