5c0529116c0b85e5e83166f6187f5dec44c54383
Victor Semionov <vsemionov@gmail.com>
2011-05-20 23:47:27 +0300

    Ordering resource IDs.

--- src/res/resource.h
+++ src/res/resource.h
@@ -3,13 +3,13 @@
 // Used by Solar Space.rc
 //
 #define IDS_DESC                        1
-#define VS_VERSION_INFO                 1
-#define IDD_CONFIG                      101
-#define IDI_APP                         102
-#define IDB_PREVIEW                     107
-#define IDD_PREVIEW                     108
-#define IDD_SHOWLOG                     109
-#define IDI_OPTIONS                     115
+#define VS_VERSION_INFO                 2
+#define IDD_PREVIEW                     101
+#define IDD_CONFIG                      102
+#define IDD_SHOWLOG                     103
+#define IDI_APP                         111
+#define IDI_OPTIONS                     112
+#define IDB_PREVIEW                     121
 #define IDC_R640                        1000
 #define IDC_R800                        1001
 #define IDC_R1024                       1002
@@ -20,17 +20,17 @@
 #define IDC_CKINFO                      1021
 #define IDC_CKCLOCK                     1022
 #define IDC_CMBSYSTEMS                  1030
-#define IDC_BLOG                        1040
 #define IDC_WEBSITE                     1050
 #define IDC_ABOUT                       1060
+#define IDC_BLOG                        1100
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_RESOURCE_VALUE        120
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1061
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_CONTROL_VALUE         1070
+#define _APS_NEXT_SYMED_VALUE           201
 #endif
 #endif
